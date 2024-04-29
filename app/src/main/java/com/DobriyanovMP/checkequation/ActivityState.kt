8133861ab595086a9c63c25e80d3c87a16eb8199
USER-MATVEY <matvey30030@gmail.com>
2024-04-29 12:00:43 +0300

    completed. WITHOUT TIMERS!

--- app/src/main/java/com/DobriyanovMP/checkequation/ActivityState.kt
+++ app/src/main/java/com/DobriyanovMP/checkequation/ActivityState.kt
@@ -1,4 +1,4 @@
-package com.DobriyanovMP.calculateEquasion
+package com.DobriyanovMP.checkequation
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
@@ -11,6 +11,11 @@ class ActivityState(
     var lastFirstOperand: Int,
     var lastSecondOperand: Int,
     var lastOperation: String,
+    var lastAnswer: Int,
+    var lastTrueAnswer: Int,
     var startButtonState: Boolean,
-    var equationColor: Int
+    var equationColor: Int,
+    var correctButtonState: Boolean,
+    var wrongButtonState: Boolean,
+    var nextButtonState: Boolean
 ): Parcelable
--- app/src/main/java/com/DobriyanovMP/checkequation/MainActivity.kt
+++ app/src/main/java/com/DobriyanovMP/checkequation/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.DobriyanovMP.checkequation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.DobriyanovMP.calculateEquasion.ActivityState
 import com.DobriyanovMP.checkequation.databinding.ActivityMainBinding
 import kotlin.math.round
 import kotlin.random.Random
@@ -19,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     private var correctAnswers = 10
     private var wrongAnswers = 20
     private var percentOfCorrect = 100.0
-    private var answer = 0
+    private var showedAnswer = 0
+    private var trueAnswer = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +35,13 @@ class MainActivity : AppCompatActivity() {
             lastFirstOperand = 0,
             lastSecondOperand = 0,
             lastOperation = "+",
+            lastAnswer = 0,
+            lastTrueAnswer = 0,
             startButtonState = true,
-            equationColor = getColor(R.color.white)
+            equationColor = getColor(R.color.white),
+            correctButtonState = false,
+            wrongButtonState = false,
+            nextButtonState = true
         )
 
         equationsCount = state.allEquationsCount
@@ -44,7 +50,12 @@ class MainActivity : AppCompatActivity() {
         firstOperand = state.lastFirstOperand
         secondOperand = state.lastSecondOperand
         operation = state.lastOperation
-        binding.StartButton.isEnabled = state.startButtonState
+        showedAnswer = state.lastAnswer
+        trueAnswer = state.lastTrueAnswer
+        binding.StartButton.visibility =
+            if (!state.startButtonState) View.GONE else View.VISIBLE
+        binding.NextButton.visibility =
+            if (!state.startButtonState) View.VISIBLE else View.GONE
         binding.EquasionLayout.setBackgroundColor(state.equationColor)
 
         setEquation()
@@ -53,10 +64,14 @@ class MainActivity : AppCompatActivity() {
         binding.AllWrongScoreField.text = wrongAnswers.toString()
         binding.PercentScore.text = "$percentOfCorrect%"
 
+        binding.CorrectButton.isEnabled = state.correctButtonState
+        binding.WrongButton.isEnabled = state.wrongButtonState
+        binding.NextButton.isEnabled = state.nextButtonState
 
         binding.StartButton.setOnClickListener { start() }
-        binding.CorrectButton.setOnClickListener { checkEquation() } // TODO: change function
-        binding.WrongButton.setOnClickListener { checkEquation() } // TODO: change function
+        binding.NextButton.setOnClickListener { createEquation() }
+        binding.CorrectButton.setOnClickListener { correctButtonPressed() }
+        binding.WrongButton.setOnClickListener { wrongButtonPressed() }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -65,14 +80,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun start() {
-        binding.StartButton.isEnabled = false
+        binding.StartButton.visibility = View.GONE
+        binding.NextButton.visibility = View.VISIBLE
         state.startButtonState = false
         createEquation()
     }
 
-    private fun createEquation() { // TODO: repurpose answer
-        var tmp: Int
+    private fun createEquation() {
+        binding.CorrectButton.isEnabled = true
+        binding.WrongButton.isEnabled = true
+        binding.NextButton.isEnabled = false
 
+        state.nextButtonState = false
+        state.correctButtonState = true
+        state.wrongButtonState = true
+
+        var tmp: Int
         val newOperation = Random.nextInt(1, 5)
 
         when (newOperation) {
@@ -98,31 +121,73 @@ class MainActivity : AppCompatActivity() {
         }
 
         when (newOperation) {
-            1 -> answer = firstOperand + secondOperand
-            2 -> answer = firstOperand - secondOperand
-            3 -> answer = firstOperand * secondOperand
-            4 -> answer = firstOperand / secondOperand
+            1 -> trueAnswer = firstOperand + secondOperand
+            2 -> trueAnswer = firstOperand - secondOperand
+            3 -> trueAnswer = firstOperand * secondOperand
+            4 -> trueAnswer = firstOperand / secondOperand
         }
+
+        if (Random.nextInt(0, 100) >= 50){
+            when (newOperation) {
+                1 -> showedAnswer = trueAnswer + Random.nextInt(10, 100)
+                2 -> showedAnswer = trueAnswer - Random.nextInt(10, 100)
+                3 -> showedAnswer = trueAnswer * Random.nextInt(2, 10)
+                4 -> showedAnswer = trueAnswer / Random.nextInt(2, 10)
+            }
+        }
+        else { showedAnswer = trueAnswer }
 
         setEquation()
     }
 
     // TODO:
-    //      divide function to: for correct and wrong
-    //      they nust block two buttons and unlock NextButton
     //      colors and stops prepares timer
-    //      function not creating nex equation
-    private fun checkEquation(){
-        if (binding.AnswerField.text.toString() == answer.toString()){
+    //      For both functions!!!
+    private fun correctButtonPressed(){
+        binding.CorrectButton.isEnabled = false
+        binding.WrongButton.isEnabled = false
+        binding.NextButton.isEnabled = true
+
+        state.nextButtonState = true
+        state.correctButtonState = false
+        state.wrongButtonState = false
+
+        if (showedAnswer == trueAnswer){
             binding.EquasionLayout.setBackgroundColor(getColor(R.color.green))
             state.equationColor = getColor(R.color.green)
             correctAnswers += 1
         }
-        else {
+        else{
             binding.EquasionLayout.setBackgroundColor(getColor(R.color.red))
             state.equationColor = getColor(R.color.red)
             wrongAnswers += 1
         }
+        updateStatistics()
+    }
+
+    private fun wrongButtonPressed(){
+        binding.CorrectButton.isEnabled = false
+        binding.WrongButton.isEnabled = false
+        binding.NextButton.isEnabled = true
+
+        state.nextButtonState = true
+        state.correctButtonState = false
+        state.wrongButtonState = false
+
+        if (showedAnswer == trueAnswer){
+            binding.EquasionLayout.setBackgroundColor(getColor(R.color.red))
+            state.equationColor = getColor(R.color.red)
+            wrongAnswers += 1
+        }
+        else{
+            binding.EquasionLayout.setBackgroundColor(getColor(R.color.green))
+            state.equationColor = getColor(R.color.green)
+            correctAnswers += 1
+        }
+        updateStatistics()
+    }
+
+    private fun updateStatistics(){
         equationsCount += 1
         percentOfCorrect = round(((correctAnswers / equationsCount.toDouble()) * 100))
 
@@ -130,25 +195,18 @@ class MainActivity : AppCompatActivity() {
         binding.AllCorrectScoreField.text = correctAnswers.toString()
         binding.AllWrongScoreField.text = wrongAnswers.toString()
         binding.PercentScore.text = "$percentOfCorrect%"
-
-        createEquation()
     }
 
-    // TODO: create function for NextButton:
-    //      locking NextButton
-    //      Create next equation
-    //      Unlocks answer buttons
-
-
-    // TODO: add answer to the list of settings
     private fun setEquation() {
         binding.Operand1.text = firstOperand.toString()
         binding.Operand2.text = secondOperand.toString()
         binding.Operation.text = operation
+        binding.AnswerField.text = showedAnswer.toString()
 
         state.lastFirstOperand = firstOperand
         state.lastSecondOperand = secondOperand
         state.lastOperation = operation
+        state.lastAnswer = showedAnswer
         state.allEquationsCount = equationsCount
         state.correctEquationsCount = correctAnswers
         state.wrongEquationsCount = wrongAnswers
